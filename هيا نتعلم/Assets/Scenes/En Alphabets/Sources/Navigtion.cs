8bd4b2c82f47c8514fccfc832b8736c15e047586
Abdulrahman-Awad21 <abdulrahman.awad21@gmail.com>
2021-12-31 02:21:29 +0200

    En Alphabets n,p Functionality

--- هيا نتعلم/Assets/Scenes/En Alphabets/Sources/Navigtion.cs
+++ هيا نتعلم/Assets/Scenes/En Alphabets/Sources/Navigtion.cs
@@ -1,15 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Navigtion : MonoBehaviour
-{
+public class Navigtion : MonoBehaviour{
+
+  private GameObject [] gO = new GameObject[26];
+  private static int x =0;
   
-  private GameObject o;
-  static int x= 1;
-  public void nextObject(){
-      Debug.Log("Button Pressed");
-      o = GameObject.FindGameObjectWithTag("BT1");
-      Debug.Log("Object mybe found ");
+
+
+  void Start(){
+
+      
+      for (int i = 1; i <= 26; i++){ 
+        gO[i-1] = GameObject.Find("Letter ("+i+")");
+      }
+      for (int i = 1; i<26 ; i++){
+        
+        gO[i].SetActive(false);
+      }
+      // string g = gO[0].name;
+      //   Debug.Log(g);
+   
+  }
+  public void nextActive(){
+    if (x<25){
+      gO[x].SetActive(false);
+      gO[x+1].SetActive(true);
+      x++;
+    }
+    else{}
+  }
+  public void previousActive(){
+    if(x>0){
+      gO[x-1].SetActive(true);
+      gO[x].SetActive(false);
+      x--;
+    }
+    else{}
   }
 }
