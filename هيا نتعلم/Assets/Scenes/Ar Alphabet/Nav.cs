b688817cdb6d1555ed33afc970991c156d980a8f
Abdulrahman-Awad21 <abdulrahman.awad21@gmail.com>
2021-12-31 14:27:49 +0200

    Upload Boda Game

---
+++ هيا نتعلم/Assets/Scenes/Ar Alphabet/Nav.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Nav : MonoBehaviour{
+
+  private GameObject [] gO = new GameObject[29];
+  private static int x =0;
+  
+
+
+  void Start(){
+
+      
+      for (int i = 0; i < 29; i++){ 
+        gO[i] = GameObject.Find("p"+i);
+      }
+      for (int i = 1; i<29 ; i++){
+        
+        gO[i].SetActive(false);
+      }
+      // string g = gO[0].name;
+      //   Debug.Log(g);
+   
+  }
+  public void nextActive(){
+    if (x<28){
+      gO[x].SetActive(false);
+      gO[x+1].SetActive(true);
+      x++;
+    }
+    else{}
+  }
+  public void previousActive(){
+    if(x>0){
+      gO[x-1].SetActive(true);
+      gO[x].SetActive(false);
+      x--;
+    }
+    else{}
+  }
+}
