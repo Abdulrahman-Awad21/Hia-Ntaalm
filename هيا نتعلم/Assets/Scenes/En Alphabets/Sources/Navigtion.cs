054a26683bcbcd0a75518ca734053e4f9584539f
Abdulrahman-Awad21 <abdulrahman.awad21@gmail.com>
2022-01-04 02:26:17 +0200

    Finish En Alphabets

--- هيا نتعلم/Assets/Scenes/En Alphabets/Sources/Navigtion.cs
+++ هيا نتعلم/Assets/Scenes/En Alphabets/Sources/Navigtion.cs
@@ -2,41 +2,84 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Navigtion : MonoBehaviour{
 
   private GameObject [] gO = new GameObject[26];
-  private static int x =0;
+  private static int x;
+  private GameObject []nav = new GameObject[2];
   
 
 
-  void Start(){
+  // void Start(){
 
       
+   
+  // }
+  
+  public void startButton(){
+      x=0;
+      nav[0] = GameObject.Find("Next");
+      nav[1] = GameObject.Find("Previous");
+      nav[1].SetActive(false);
+
       for (int i = 1; i <= 26; i++){ 
         gO[i-1] = GameObject.Find("Letter ("+i+")");
       }
+
       for (int i = 1; i<26 ; i++){
         
         gO[i].SetActive(false);
       }
-      // string g = gO[0].name;
-      //   Debug.Log(g);
-   
+
+  }
+
+  public void reloadScene(){
+    SceneManager.LoadScene("En Alphabets");
+  }
+  public void mainMenu(){
+    SceneManager.LoadScene("Main Menu Scene");
   }
   public void nextActive(){
-    if (x<25){
-      gO[x].SetActive(false);
-      gO[x+1].SetActive(true);
-      x++;
+    x++;
+    Debug.Log(x+"");
+    
+    if (x == 0){
+      nav[1].SetActive(false);
+      nav[0].SetActive(true);
+    }
+    else if (x == 25){
+      nav[0].SetActive(false);
+    }
+    else{
+      nav[1].SetActive(true);
+      nav[0].SetActive(true);
+    }
+    
+    if (x>0 && x<=26){
+      gO[x-1].SetActive(false);
+      gO[x].SetActive(true);
+      
     }
     else{}
+
+    
   }
   public void previousActive(){
-    if(x>0){
-      gO[x-1].SetActive(true);
-      gO[x].SetActive(false);
-      x--;
+    x--;
+    Debug.Log(x+"");
+    if (x == 0){
+      nav[1].SetActive(false);
+    }
+    else {
+      nav[0].SetActive(true);
+      nav[1].SetActive(true);
+    }
+    if(x>=0){
+      gO[x].SetActive(true);
+      gO[x+1].SetActive(false);
+      
     }
     else{}
   }
