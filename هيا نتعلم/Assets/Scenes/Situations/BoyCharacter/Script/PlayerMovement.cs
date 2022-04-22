f5eb9f1a765b12b2e3fdb666eafe959808ac1912
Abdulrahman-Awad21 <abdulrahman.awad21@gmail.com>
2022-04-22 19:13:42 +0200

    Add PlayerPrefab

---
+++ هيا نتعلم/Assets/Scenes/Situations/BoyCharacter/Script/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed ;
+    private Animator playerAnimator;
+    private  float horizintalValue;
+
+    private void Awake() {
+        playerAnimator = GetComponentInChildren<Animator>();
+
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        horizintalValue = Input.GetAxis("Horizontal");
+        move();
+    }
+
+    void move(){
+        if (horizintalValue >0)
+            playerAnimator.SetBool("Walking Right" , true);
+        else if (horizintalValue <0)
+            playerAnimator.SetBool("Walking Left" , true);
+        else{ 
+            playerAnimator.SetBool("Walking Right" , false);
+            playerAnimator.SetBool("Walking Left" , false);
+            }
+    }
+    
+
+}
