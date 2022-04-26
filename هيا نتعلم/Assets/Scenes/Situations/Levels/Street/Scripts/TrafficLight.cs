4ec7b3efaf5208c7f906aad296a19ff4b530ffa3
Abdulrahman-Awad21 <abdulrahman.awad21@gmail.com>
2022-04-26 23:10:42 +0200

    add happy jump animation

--- هيا نتعلم/Assets/Scenes/Situations/Levels/Street/Scripts/TrafficLight.cs
+++ هيا نتعلم/Assets/Scenes/Situations/Levels/Street/Scripts/TrafficLight.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class TrafficLight : MonoBehaviour
 {
    public GameObject light1,light2;
-   private float switchRate = 2f;
+   public float switchRate = 2f;
    private float nextLight;
    private bool gate = true;
 
    
    private void Update() {
        if (Time.time >=nextLight){
-           Debug.Log (nextLight);
+           
             nextLight = Time.time + switchRate;
             if(gate){
                 light1.SetActive(true);
