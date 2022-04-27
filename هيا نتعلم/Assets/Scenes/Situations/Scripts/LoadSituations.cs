b25ab78184cf2d47faa9c5b1a3f48c5eaa3416ce
Abdelrahman-Mostafa25 <88713984+Abdelrahman-Mostafa25@users.noreply.github.com>
2022-04-27 16:25:03 +0200

    Situations_UI

---
+++ هيا نتعلم/Assets/Scenes/Situations/Scripts/LoadSituations.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSituations : MonoBehaviour
+{
+    public void playHome()
+    {
+        SceneManager.LoadScene("home");
+    }
+    public void playSchool()
+    {
+        SceneManager.LoadScene("School");
+    }
+    public void playStreet()
+    {
+        SceneManager.LoadScene("Streat");
+    }
+
+}
